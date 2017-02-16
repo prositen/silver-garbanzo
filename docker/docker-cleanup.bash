4ee31b32e904c2b6600919e6fbd39ab4497bf582
magnus-lyrberg <magnus-lyrberg@users.noreply.github.com>
2017-02-16 10:19:21 +0100

    Added comment about prune

--- docker/docker-cleanup.bash
+++ docker/docker-cleanup.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# From Docker 1.13 docker prune is available
+# docker system prune
+
 # Deletes all exited docker containers and dangling images
 docker ps -q -f status=exited | xargs --no-run-if-empty docker rm
 docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi
